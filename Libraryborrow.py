f18051ef5e39a94cb078458c2af959e6c9d21170
khadijahmuslim <58142719+khadijahmuslim@users.noreply.github.com>
2026-08-27 12:22:24 +0800

    Create Libraryborrow.py
    A dictionary to track each book's status: Borrow book, check system, available, days

---
+++ Libraryborrow.py
@@ -0,0 +1,23 @@
+""" - Library tracking system -
+A dictionary to track each book's status: Borrow book, check system, available, days"""
+
+class LibraryTracking():
+  def __init__(self):
+    self.name_book = []
+    self.available =
+    
+  def searchbook():
+    name_book = ["Electronic Fundamental", "Computer Architecture", "101 Bios"]
+    for namebook in name_book :
+      book_status = self.availablity(namebook)
+
+  def availability():
+    status = ""
+    if status:
+      print(f"Your book is not available. Someone is already borrow the book")
+    
+
+track_book = LibraryTracking()
+track_book.searchbook()
+print(f"Please enter your book name to check the availability : {name_book}")
+
